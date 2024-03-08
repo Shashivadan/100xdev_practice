e481cdf93080a0828dbb7a61ac1d543c642de633
Shashivadan <shashivadan99@gmail.com>
2024-03-08 21:44:32 +0530

    👾🦄

--- week-9/typescript/first.js
+++ week-9/typescript/first.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const x = 1;
 const y = "hello";
 console.log(x, y);
@@ -17,4 +18,26 @@ function isLegal(age) {
     return false;
 }
 console.log(isLegal(48));
+const arr = ["shsashi", 1, 2, 3];
+arr.push("shashi");
+console.log("first", arr[0]);
+const ob = {
+    name: "shashi",
+};
 Hello("world");
+let any = "shashi"; // big no for "any"  and fetch request gives any /
+const iscoder = true;
+const person = {
+    name: "shsahi",
+    age: 22,
+    iscoder,
+    fav: ["shashi", "vadan", 1, 3, 3],
+};
+const student = {
+    name: "shashivadan",
+    PNo: 6281373269,
+};
+console.log("student", student);
+function logName(name) {
+    console.log(name);
+}
--- week-9/typescript/first.ts
+++ week-9/typescript/first.ts
@@ -1,3 +1,5 @@
+import { number, string } from "zod";
+
 const x: number = 1;
 const y: string = "hello";
 console.log(x, y);
@@ -8,13 +10,10 @@ function Hello(name: string) {
   console.log("hello", name);
 }
 
-
-function 
-
 function sum(a: number, b: number): number {
   return a + b;
 }
-Q
+
 console.log("first", sum(1, 2));
 
 function isLegal(age: number): boolean {
@@ -26,4 +25,64 @@ function isLegal(age: number): boolean {
 
 console.log(isLegal(48));
 
+const arr: (string | number)[] = ["shsashi", 1, 2, 3];
+
+arr.push("shashi");
+
+console.log("first", arr[0]);
+
+const ob: object = {
+  name: "shashi",
+};
+
 Hello("world");
+
+let any: any = "shashi"; // big no for "any"  and fetch request gives any /
+
+//every impotent
+type PersonName = {
+  name: string;
+  age: number;
+  iscoder?: boolean;
+  fav: (string | number)[];
+};
+
+const iscoder: boolean = true;
+
+const person: PersonName = {
+  name: "shsahi",
+  age: 22,
+  iscoder,
+  fav: ["shashi", "vadan", 1, 3, 3],
+};
+
+interface StudentType {
+  name: string;
+  PNo: number;
+}
+
+const student: StudentType = {
+  name: "shashivadan",
+  PNo: 6281373269,
+};
+
+console.log("student", student);
+
+function logName(name: string): void {
+  console.log(name);
+}
+
+function names(r: string): string {
+  return r;
+}
+
+type Names = {
+  hello: string;
+  nn?: string;
+  hu: number;
+};
+
+const hehe: Names = {
+  hello: "shsahsi",
+  hu: 43,
+};
