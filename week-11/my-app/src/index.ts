4ebe8c12a8e2bb86636f71ea0826010c320e488a
Shashivadan <shashivadan99@gmail.com>
2024-04-10 15:39:02 +0530

    added: week-11 and serverless backends

---
+++ week-11/my-app/src/index.ts
@@ -0,0 +1,19 @@
+export interface Env {}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+
+		if (request.method === 'GET') {
+			return Response.json({
+				message: 'you sent a get request',
+				name: 'shashi vadan',
+			});
+		} else {
+			return Response.json({
+				message: 'you did not send a get request',
+			});
+		}
+	},
+};
